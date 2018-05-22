1ffa9c8e4d7348b320b91252ea52a6ed66269710
sharanya <skommareddy@horizon.csueastbay.edu>
2018-05-22 09:34:30 -0700

    Update

---
+++ controllers/database.js
@@ -0,0 +1,59 @@
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://sharanya16:sAsh5+enkA@ds225840.mlab.com:25840/heroku_mgjkmjm6';
+
+
+/** getAllRoutes controller logic that current does model logic too -connects to Mongo database and
+ * queries the Routes collection to retrieve all the routes and build the output usig the
+ * ejs template mongodb.ejs found in views directory
+ * @param request
+ * @param response
+ *
+ */
+module.exports.getAllOrders =  function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err,  client) {
+        if(err) throw err;
+
+
+        //get handle to the databse
+        var theDatabase = client.db('heroku_mgjkmjm6');
+
+
+        //get collection of routes
+        var Orders = theDatabase.collection('ORDERS');
+
+
+        //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
+        //   this request and loop  is to display content in the  console log
+        var c = Orders.find({}); //
+
+        c.forEach(
+            function(myDoc) {
+                console.log( "name: " + myDoc.name );  //just  loging the output to the console
+            }
+        );
+
+
+        //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
+        //   array called docs that you  forward to the  getAllRoutes.ejs view for use there
+        Orders.find().toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('getAllOrders', {results: docs});
+
+        });
+
+
+        //Showing in comments here some alternative read (find) requests
+        //this gets Routes where frequency>=10 and sorts by name
+        // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
+        // this sorts all the Routes by name
+        //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
+
+
+        //close connection when your app is terminating.
+        client.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+};//end function
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+mongodb = require('mongodb');
+
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://sharanya16:sAsh5+enkA@ds225840.mlab.com:25840/heroku_mgjkmjm6';
+
+
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Routes collection
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+
+
+
+
+//CODE to route /getAllRoutes to appropriate  Controller function
+//**************************************************************************
+//***** mongodb get all of the Routes in Routes collection w
+//      and Render information iwith an ejs view
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
 
 module.exports = router;
