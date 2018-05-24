b960dc18b9f662d49c79ac65f3e81e4712c4918d
sharanya <skommareddy@horizon.csueastbay.edu>
2018-05-24 12:36:01 -0700

    new controller

--- controllers/database.js
+++ controllers/database.js
@@ -8,9 +8,6 @@ var mongoDBURI = process.env.MONGODB_URI || 'mongodb://sharanya16:sAsh5+enkA@ds2
  * @param request
  * @param response
  *
- *
- *
- * 
  */
 module.exports.getAllOrders =  function (request, response) {
 
