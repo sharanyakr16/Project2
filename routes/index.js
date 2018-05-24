c3661abe5fe4b3db49f685514751252cf1e99882
sharanya <skommareddy@horizon.csueastbay.edu>
2018-05-23 21:04:51 -0700

    Update index.js with storeData

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,9 @@ router.get('/', function(req, res, next) {
 router.post('/storeData', function(req, res, next) {
 //expecting data variable called order--retrieve value using body-parser
     var value_name = req.body.order  //retrieve the data associated with order
+    var value_name1 = req.body.customer
     res.send("order succesfully received: " + value_name);
+    res.send("order succesfully received: " + value_name1);
 });
 
 
